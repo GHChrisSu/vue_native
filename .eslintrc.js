17519840ba603a95d6554483d047e6c7aecbd394
suhao <chrissusuhao@gmail.com>
2021-05-07 13:55:43 +0900

    rxdb compile

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  env: {
+    node: true
+  },
+  extends: [
+    '@react-native-community',
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
 };
--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -1,8 +1,17 @@
 // Rename this sample file to main.js to use on your project.
 // The main.js file will be overwritten in updates/reinstalls.
-var Realm = require('realm');
 
-console.log(Realm);
+import { createRxDatabase } from 'rxdb';
+createRxDatabase({
+  name: 'heroesdb',           // <- name
+  adapter: 'idb',          // <- storage-adapter
+  password: 'myPassword',     // <- password (optional)
+  multiInstance: true,         // <- multiInstance (optional, default: true)
+  eventReduce: false // <- eventReduce (optional, default: true)
+}).then((db) => {
+  console.dir(db);
+});
+
 var rn_bridge = require('rn-bridge');
 
 // Echo every message received from react-native.
